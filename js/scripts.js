71cd3819e61588b2524d425235057aa590b57e64
Jun C <kjstudios858@gmail.com>
2019-01-05 23:14:32 -0800

    refactor Add calculations

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
 new Vue({
     el: "#app",
     data: {
-        hello: "Hello!"
+        sensorDimension: 0,
+        focalLength: 0,
+        focalLengthTelescope: 0,
+        apertureOfTelescope: 0,
+        reducerAperture: 0
+    },
+    computed: {
+        fieldOfView: function() {
+            return (( 3436 * this.sensorDimension) / this.focalLength);
+        },
+        focalLengthReducer: function() {
+            return (( this.focalLengthTelescope * this.reducerAperture) / this.apertureOfTelescope);
+        }
     }
-})
+    //     showFov: function() {
+    //         console.log(isNaN(this.sensorDimension));
+    //         console.log(isNaN(this.focalLength));
+
+    //         if ( isNaN(this.sensorDimension) && isNaN(this.focalLength) ) {
+    //             return false;
+    //         } else {
+    //             return true;
+    //         }
+    //     }
+    // }
+});
